659a35aeed6abad106b29152c951fb6f194141a7
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-18 21:26:19 +0200

    edit progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,3 @@
 export default function ProgressBar(min, max, value) {
-  return (
-    <header className="progress">
-      <progress min={min} max={max} value={value}></progress>
-    </header>
-  );
+  return <progress min={min} max={max} value={value}></progress>;
 }
