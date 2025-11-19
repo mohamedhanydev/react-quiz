cd6d278857926ef76de866382c92d94dd3412bde
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-19 13:21:47 +0200

    Edit start component

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,9 @@
-export default function Start(onClick) {
+export default function Start({ onClick }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>15 questions to test your React mastery</h3>
-      <button class="btn btn-ui" onClick={() => onClick({ type: "start" })}>
+      <button className="btn btn-ui" onClick={() => onClick(true)}>
         Let's start
       </button>
     </div>
